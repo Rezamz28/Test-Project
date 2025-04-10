56defa7c8c39fdad1f25e80eba49a9d820b4d96d
Rezamz28 <moazezi.reza8@gmail.com>
2025-04-10 15:59:51 +0200

    modified link

--- App.py
+++ App.py
@@ -3,5 +3,5 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import seaborn as sns
 
-df = pd.read_csv('C:\Users\moaze\OneDrive\Dokumente\VSCode-Project\homicide_project\homicide_by_countries.csv')
+df = pd.read_csv('homicide_by_countries.csv')
 print(df)
