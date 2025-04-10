80c34f7430a8811bbe022cc2bfda38303f72ce7d
Rezamz28 <moazezi.reza8@gmail.com>
2025-04-10 17:46:37 +0200

    Data Processing

--- App.py
+++ App.py
@@ -5,3 +5,14 @@ import seaborn as sns
 
 df = pd.read_csv('homicide_by_countries.csv')
 print(df)
+print(df.shape)
+print(df.isnull().sum())
+df.dropna(inplace=True)
+df['Rate'] = df['Rate'].astype(int)
+print(df)
+
+my_list = ['Rate','Count','Year']
+for i in my_list:
+    df[i] = df[i].astype(int)
+
+print(df)
